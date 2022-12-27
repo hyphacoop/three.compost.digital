cfc30c144c4a1189158a82b591d15457cc42bddc
f <f@sutty.nl>
2022-12-27 11:44:09 -0300

    feat: entry.js

---
+++ _packs/entry.js
@@ -0,0 +1,27 @@
+import DeviceDetector from "device-detector-js";
+import { Notifier } from "@airbrake/browser";
+
+window.deviceDetector = new DeviceDetector();
+window.device = window.deviceDetector.parse(navigator.userAgent);
+
+if (!window.device.bot && window.env.JEKYLL_ENV === "production") {
+  window.airbrake = new Notifier({
+    projectId: window.env.AIRBRAKE_PROJECT_ID,
+    projectKey: window.env.AIRBRAKE_PROJECT_KEY,
+    host: "https://panel.sutty.nl",
+  });
+
+  console.originalError = console.error;
+  console.error = (...e) => {
+    window.airbrake.notify(e.join(" "));
+    return console.originalError(...e);
+  };
+}
+
+import * as Turbo from "@hotwired/turbo";
+Turbo.start();
+
+import { Application } from "@hotwired/stimulus";
+window.Stimulus = Application.start();
+
+Stimulus.debug = (window.env.JEKYLL_ENV !== "production");
