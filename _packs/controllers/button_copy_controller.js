c4fc2777f0e7081ccf1fae6723f79d32d8852c07
f <f@sutty.nl>
2023-01-06 16:36:38 -0300

    feat: icons

--- _packs/controllers/button_copy_controller.js
+++ _packs/controllers/button_copy_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["text", "icon", "confirmationIcon"];
   static values = {
     text: String,
     confirmation: String,
@@ -11,9 +12,16 @@ export default class extends Controller {
 
     try {
       await navigator.clipboard.writeText(this.textValue.trim());
-
-      this.element.innerText = this.confirmationValue;
     } catch(e) {
+      console.error("Couldn't copy", e);
+      return;
     }
+
+    const textTarget = this.hasTextTarget ? "textTarget" : "element";
+
+    this[textTarget].innerText = this.confirmationValue;
+
+    if (this.hasIconTarget) this.iconTarget.classList.add("d-none");
+    if (this.hasConfirmationIconTarget) this.confirmationIconTarget.classList.remove("d-none");
   }
 }
